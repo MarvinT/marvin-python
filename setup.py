314fc4b90f8c32985f489b86722d13d40626bc0b
Marvin T <MarvinT@users.noreply.github.com>
2017-09-17 14:29:45 -0700

    setup package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='marvin',
+    version='0.0.1',
+    description='python utils',
+    author='Marvin Thielk',
+    author_email='marvin.thielk@gmail.com',
+    packages=['marvin',
+              ],
+)
