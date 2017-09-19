38181bd64cfe0e3fc3c9654946de6fa66cc94fa1
Marvin T <MarvinT@users.noreply.github.com>
2017-09-18 19:26:50 -0700

    save fig options

--- marvin/figure_utils.py
+++ marvin/figure_utils.py
@@ -1,2 +1,2 @@
 def savefig(g, name, folder='Figures/', format='eps'):
-    g.savefig(folder+name+'.'+format, format=format)
+    g.savefig(folder+name+'.'+format, format=format, bbox_inches='tight', transparent=True, pad_inches=0)
