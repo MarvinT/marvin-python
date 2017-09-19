7983e79dda48e70711e6b81e73e75fd5b0053645
Marvin T <MarvinT@users.noreply.github.com>
2017-09-18 19:46:49 -0700

    default pdf

--- marvin/figure_utils.py
+++ marvin/figure_utils.py
@@ -1,2 +1,2 @@
-def savefig(g, name, folder='Figures/', format='eps'):
+def savefig(g, name, folder='Figures/', format='pdf'):
     g.savefig(folder+name+'.'+format, format=format, bbox_inches='tight', transparent=True, pad_inches=0)
