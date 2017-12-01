2bc6e5dc43e2f76b2c5475cae820c35a38c4015a
Marvin Thielk <MarvinT@github.com>
2017-11-30 20:19:44 -0800

    recursive ensure folder exists

--- marvin/folder_utils.py
+++ marvin/folder_utils.py
@@ -1,6 +1,7 @@
 from os import mkdir
-from os.path import exists
+from os.path import exists, split
 
 def ensure_folder_exists(folder):
     if not exists(folder):
+        ensure_folder_exists(split(folder)[0])
         mkdir(folder)
