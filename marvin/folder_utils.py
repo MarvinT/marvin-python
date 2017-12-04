3c71e36a1fdbed8b970ad21a43ad8da7894728cf
Marvin Thielk <MarvinT@github.com>
2017-12-04 14:27:48 -0800

    threadsafing ensure_folder_exists

--- marvin/folder_utils.py
+++ marvin/folder_utils.py
@@ -4,4 +4,8 @@ from os.path import exists, split
 def ensure_folder_exists(folder):
     if not exists(folder):
         ensure_folder_exists(split(folder)[0])
-        mkdir(folder)
+        try:
+            mkdir(folder)
+        except OSError as e:
+            if e.errno != 17:
+                raise e
