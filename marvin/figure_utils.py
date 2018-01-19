9188ff0b7af9efd07a764c4ad7df1c9812351466
Marvin T <MarvinT@users.noreply.github.com>
2018-01-19 01:30:03 -0800

    transparent options

--- marvin/figure_utils.py
+++ marvin/figure_utils.py
@@ -1,2 +1,2 @@
-def savefig(g, name, folder='Figures/', format='pdf'):
-    g.savefig(folder+name+'.'+format, format=format, bbox_inches='tight', transparent=True, pad_inches=0)
+def savefig(g, name, folder='Figures/', format='pdf', transparent=True):
+    g.savefig(folder+name+'.'+format, format=format, bbox_inches='tight', transparent=transparent, pad_inches=0)
