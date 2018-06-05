3dc1720cac35bbd86eaad501fbc9c646e06d0482
Marvin T <MarvinT@users.noreply.github.com>
2018-06-05 05:36:25 -0700

    savefig saves several formats by default now

--- marvin/figure_utils.py
+++ marvin/figure_utils.py
@@ -1,2 +1,5 @@
-def savefig(g, name, folder='Figures/', format='pdf', bbox_inches='tight', transparent=True, pad_inches=0):
-    g.savefig(folder+name+'.'+format, format=format, bbox_inches=bbox_inches, transparent=transparent, pad_inches=pad_inches)
+def savefig(g, name, folder='Figures/', format=None, formats=['png', 'pdf', 'svg', 'eps'], bbox_inches='tight', transparent=True, pad_inches=0):
+	if format:
+		formats = [format]
+	for format in formats:
+	    g.savefig(folder+name+'.'+format, format=format, bbox_inches=bbox_inches, transparent=transparent, pad_inches=pad_inches)
